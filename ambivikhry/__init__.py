0b9bcf48717fe2429016848b876bac24a9dcde1e
messerzai <messsssssser@gmail.com>
2026-09-20 09:31:21 +0300

    feat: expose bounded self-model

--- ambivikhry/__init__.py
+++ ambivikhry/__init__.py
@@ -6,5 +6,6 @@ from .policy import PolicyGate
 from .verifier import Verifier, VerificationReport
 from .replication import Replicator, ReplicationPolicy
 from .trajectory import TrajectoryStep, lineage_digest, validate_step
+from .self_model import SelfModel, current_self_model, next_iteration_goal
 
-__version__ = "2.12.0"
+__version__ = "2.13.0"
