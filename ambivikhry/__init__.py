4280080452f3fb818d7ed000f7bfa047d5e62e13
messerzai <messsssssser@gmail.com>
2026-09-20 09:34:00 +0300

    v2.14: export evidence chain and bump version

--- ambivikhry/__init__.py
+++ ambivikhry/__init__.py
@@ -7,5 +7,6 @@ from .verifier import Verifier, VerificationReport
 from .replication import Replicator, ReplicationPolicy
 from .trajectory import TrajectoryStep, lineage_digest, validate_step
 from .self_model import SelfModel, current_self_model, next_iteration_goal
+from .evidence_chain import EvidenceRecord, improvement_claim, validate_evidence
 
-__version__ = "2.13.0"
+__version__ = "2.14.0"
