6f5661a8ec6c3341045a427943b6b5e40805ac1e
messerzai <messsssssser@gmail.com>
2026-09-22 08:36:38 +0300

    feat: expose controlled comfort-expansion policy

--- ambivikhry/__init__.py
+++ ambivikhry/__init__.py
@@ -5,5 +5,6 @@ from .tools import Tool, ToolRegistry, DryRunAdapter
 from .policy import PolicyGate
 from .verifier import Verifier, VerificationReport
 from .replication import Replicator, ReplicationPolicy
+from .comfort_expansion import ComfortContext, ComfortAssessment, ComfortExpansionPolicy
 
 __version__ = "2.6.0"
